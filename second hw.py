aa8d410d1e5a21ec89603cecf2b38455c34fb3a0
163ilya <147040272+163ilya@users.noreply.github.com>
2023-10-07 12:06:50 +0300

    Create second hw.py

---
+++ second hw.py
@@ -0,0 +1,9 @@
+# задание 1
+a = float(input('Введите число: '))
+b = float(input('Введите другое число: '))
+result = (a / b) * 100
+print(round(result, 2))
+
+# задание 2
+for i in range(1, 11):
+    print(i)
