8b65dc857b11e3569a7f900dea0a142adb285397
163ilya <147040272+163ilya@users.noreply.github.com>
2023-10-16 21:31:54 +0300

    Update fourth hw.py

--- fourth hw.py
+++ fourth hw.py
@@ -28,9 +28,23 @@ y = "y"
 result = f"x: {КоличествоX}, y: {КоличествоY}"
 print(result)
 
-# задача 6 - решил странно, понятно только так
-# если есть подсказка как использовать find, прикрепите пожалуйства
-text = "When he saw Sally (a girl he used to go to school with) in the shop, he could not believe his eyes. She was fantastic (as always)!"
-text.find("(a girl he used to go to school with)" and "(as always)")
-print("a girl he used to go to school with")
-print("as always")
+# задача 6
+
+s = input("Введите строку: ")
+
+line=""
+k=0
+for i in s:
+   if (i=="("):
+       k=1
+   if (k==1):
+       line=line+i
+   if (i==")"):
+       k=0
+def find_substring(s, left_bracket, right_bracket):
+    left_bracket_index = s.find(left_bracket)
+    right_bracket_index = s.find(right_bracket, left_bracket_index + 1)
+    substring = s[left_bracket_index + 1: right_bracket_index]
+    return substring
+substring = find_substring(s, '(', ')')
+print (f" {substring}")
