b55b4219c3ae09aea9a6772bcc8b21336b505ed7
163ilya <147040272+163ilya@users.noreply.github.com>
2023-11-10 22:23:55 +0300

    Update seventh hw.py

--- seventh hw.py
+++ seventh hw.py
@@ -1,10 +1,18 @@
 # задание 1
 list1 = [1, 2, 3, 4]
-list2 = [5, 6, 7, 8]
+list2 = [5, 6, 7, 8, 9]
 
-result = list(map(lambda x, y: x + y, list1, list2))
+max_len = max(len(list1), len(list2))
+
+result = [
+    list1[i] + list2[i] if i < len(list1) and i < len(list2)
+    else list1[i] if i < len(list1)
+    else list2[i]
+    for i in range(max_len)
+]
 
 print(result)
+
 
 # задание 2
 numbers = [17, 19, 24, 39, 65, 78, 91, 104, 117, 133] # можно использовать и другие цифры
