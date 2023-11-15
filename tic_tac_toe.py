0e54c22ef1a75ab2a12de62b0e0edb242f883591
163ilya <147040272+163ilya@users.noreply.github.com>
2023-11-15 21:47:37 +0300

    tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,17 +4,20 @@ def draw_board(board):
     for i in range(0, 9, 3):
         print('|', board[i], '|', board[i + 1], '|', board[i + 2], '|')
 
+def is_cell_occupied(board, i):
+    return board[int(i)] in ["X", "O"]
+
 def get_player_move(board, symbol):
     i = input(f"Введите номер ячейки, в которую хотите поставить свой {symbol}")
-    while (i not in ['0', '1', '2', '3', '4', '5', '6', '7', '8']) or (board[int(i)] in "XO"):
+    while (i not in ['0', '1', '2', '3', '4', '5', '6', '7', '8']) or is_cell_occupied(board, i):
         if i not in ['0', '1', '2', '3', '4', '5', '6', '7', '8']:
             i = input("Введите цифру от 0 до 8\n")
-        elif board[int(i)] in "XO":
+        elif is_cell_occupied(board, i):
             i = input("К сожалению, эта ячейка занята, выберите любую свободную\n")
     return int(i)
 
 def get_computer_move(board, symbol):
-    free_i = [i for i in range(len(board)) if board[i] not in "XO"]
+    free_i = [i for i in range(len(board)) if board[i] not in ["X", "O"]]
     if not free_i:
         return None
     return random.choice(free_i)
@@ -35,6 +38,9 @@ def check_win(board, symbol):
         return True
     return False
 
+def is_board_full(board):
+    return all(cell in ["X", "O"] for cell in board)
+
 def main():
     board = [str(i) for i in range(9)]
     computer_symbol = 'X'
@@ -50,12 +56,17 @@ def main():
         draw_board(board)
         if check_win(board, computer_symbol):
             break
+        if is_board_full(board):
+            print("Ничья! Игра завершена.")
+            break
         player_i = get_player_move(board, player_symbol)
         board[player_i] = player_symbol
         print("Доска после хода игрока")
         draw_board(board)
         if check_win(board, player_symbol):
             break
+        if is_board_full(board):
+            print("Ничья! Игра завершена.")
+            break
 
 main()
-
