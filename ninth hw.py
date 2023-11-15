b45613aa79fbf06ed415acb7c987c10980e30867
163ilya <147040272+163ilya@users.noreply.github.com>
2023-11-15 22:36:02 +0300

    ninth hw.py

--- ninth hw.py
+++ ninth hw.py
@@ -1,48 +1,59 @@
 # Задание 1
 
-def nod(x, y):
+def find_nod(x, y):
     while y:
         x, y = y, x % y
     return x
 
-def nok(a, b):
-    return abs(a * b) // nod(a, b)
+def find_nok(a, b):
+    return abs(a * b) / find_nod(a, b)
 
+# Ввод двух чисел от пользователя
 a = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
 
-result = nok(a, b)
-print(f"Наименьшее общее кратное чисел {a} и {b} равно {result}.")
+# Находим и выводим НОК
+lcm = find_nok(a, b)
+print("Наименьшее общее кратное чисел", a, "и", b, "равно", int(lcm))
 
 
 # Задание 2
 
-def find_simple(n):
-    simple = []
-    for i in range(2, n + 1):
-        is_simple = True
-        for delit in range(2, int(i**0.5) + 1):
-            if i % delit == 0:
-                is_simple = False
-                break
-        if is_simple:
-            simple.append(i)
-    return simple
+def prostoe(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
+def find_prostoe(n):
+    primes = [num for num in range(2, n + 1) if prostoe(num)]
+    return primes
+
+# Ввод числа n от пользователя
 n = int(input("Введите число n: "))
 
-simple_numbers = find_simple(n)
-print(f"Простые числа от 1 до {n}: {simple_numbers}")
+# Находим и выводим простые числа от 1 до n
+prime_numbers = find_prostoe(n)
+print(f"Простые числа от 1 до {n}: {prime_numbers}")
+
 
 
 # Задание 3
 
-def find_delit(n):
-    delit = [i for i in range(1, n + 1) if n % i == 0]
-    return delit
+def find_split(n):
+    split = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            split.append(i)
+    return split
 
 n = int(input("Введите число n: "))
 
-delit = find_delit(n)
-print(f"Делители числа {n}: {delit}")
+split_list = find_split(n)
+print("Делители числа {}: {}".format(n, split_list))
+
+
+
 
